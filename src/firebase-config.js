4c3857c9abd9c16d59ef8e6187c65090e7c35e85
Muriely24 <157389369+Muriely24@users.noreply.github.com>
2024-02-26 13:19:35 -0500

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,13 @@ import {getFirestore} from '@firebase/firestore'
  
 
 const firebaseConfig = { 
-    apiKey: "AIzaSyD4-0TGuz4qrp56_gKD7XkOoyckXNCn2OA", 
-    authDomain: "customers-test-f10df.firebaseapp.com", 
-    projectId: "customers-test-f10df", 
-    storageBucket: "customers-test-f10df.appspot.com", 
-    messagingSenderId: "857205683642", 
-    appId: "1:857205683642:web:b831663382c8cba3ead983", 
-    measurementId: "G-YYFQMS578D" 
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 
   }; 
 
